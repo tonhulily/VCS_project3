f220711001d7a99f3586d29f798ea29ae4028787
tonhulily <tonhu170605@gmail.com>
2026-03-13 11:24:48 +0700

    fix installed features

--- app/src/main/java/com/example/vcs_project3/BackgroundService.kt
+++ app/src/main/java/com/example/vcs_project3/BackgroundService.kt
@@ -1,13 +1,22 @@
 package com.example.vcs_project3
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class BackgroundService : Service() {
     private lateinit var screenReceiver: ScreenReceiver
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var packageManagerRef: PackageManager
+    private val installedPackages = HashSet<String>()
 
     override fun onCreate() {
         super.onCreate()
@@ -21,11 +30,56 @@ class BackgroundService : Service() {
 
         registerReceiver(screenReceiver, filter)
         Log.d("SERVICE_TEST", "ScreenReceiver registered")
+
+        packageManagerRef = packageManager
+
+        val apps = packageManagerRef.getInstalledApplications(0)
+        for (app in apps) {
+            installedPackages.add(app.packageName)
+        }
+        startCheckingInstalledApps()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("SERVICE_TEST", "BackgroundService is running")
         return START_STICKY
+    }
+    private fun startCheckingInstalledApps() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val apps = packageManagerRef.getInstalledApplications(0)
+                for (app in apps) {
+                    if (!installedPackages.contains(app.packageName)) {
+                        installedPackages.add(app.packageName)
+                        Log.d("INSTALL_TEST", "New app detected: ${app.packageName}")
+                        showInstallNotification(app.packageName)
+                    }
+                }
+                handler.postDelayed(this, 3000)
+            }
+
+        }, 3000)
+    }
+    private fun showInstallNotification(packageName: String) {
+        val channelId = "install_channel"
+        val manager =
+            getSystemService(NotificationManager::class.java)
+
+        val channel = NotificationChannel(
+            channelId,
+            "Install Notification",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        manager.createNotificationChannel(channel)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("App Installed")
+            .setContentText("New app installed: $packageName")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .build()
+
+        manager.notify(packageName.hashCode(), notification)
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/vcs_project3/MainActivity.kt
+++ app/src/main/java/com/example/vcs_project3/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.vcs_project3
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,19 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, BackgroundService::class.java)
 
         startService(intent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1
+                )
+            }
+        }
     }
 }
